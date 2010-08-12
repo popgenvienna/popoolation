5ee67e4a12daba4c976d9ba5ce69217f44b35d5e
Robert Kofler <rokofler@gmail.com>
2010-08-12 15:54:36 +0000

    fixed errror in calculate-dxy.pl

--- calculate-dxy.pl
+++ calculate-dxy.pl
@@ -34,6 +34,7 @@ GetOptions(
 
 pod2usage(-verbose=>2) if $help;
 Test::runTests() if $test;
+#die "Tests have not yet been implemented" if $test;
 
    
 pod2usage(-msg=>"A input file has to be provided\n",-verbose=>1) unless -e $input;
@@ -49,9 +50,19 @@ my $reader;
 
 $reader=BpSlider->new($input,$windowsize,$step);
 
+# population count
+my $speciesCount=0;
+
+$speciesCount=$reader->count_samples();
+
+if($speciesCount !=3) {
+	
+	pod2usage(-msg=>"Only 3 species are allowed to calculate divergence\n",-verbose=>1);
+}
 
 print "\n\nCalculating Dxy started at\t". localtime() ." ....\n";
 
+#exit;
 
 
 while(my $window=$reader->nextWindow())
@@ -68,15 +79,26 @@ while(my $window=$reader->nextWindow())
 
         if ($coveredFrac>=$minCoverageFraction) {
 	    
-	    my($string)=Utility::calculateDxy($data,$pos,$chr);
+	    my($string)=Utility::calculateDxy($data,$pos,$chr,$win,$minCoverageFraction);
 	    #print "$coveredFrac\t$minCoverageFraction\n";
 	    print $ofh "$string\n";
-            #print "$string\n";
+            print "$string\n";
 	    
 	}
         else {
-            print $ofh "$chr\t$pos\tna\tna\tna\n";
-            #print "$chr\t$pos\tna\tna\tna\n";
+		
+		my $considered_pos = 0;
+		foreach my $d (@$data)
+		{
+		    ### if condition to discard the position with gap (-) in any of 3 species
+		    if (("$d->{sp1}" ne "-") and ("$d->{sp2}" ne "-") and ("$d->{sp3}" ne "-")) {
+			if (($d->{sp1} =~ m/N/i) and ($d->{sp2} =~ m/N/i) and ($d->{sp3} =~ m/N/i)) {
+				$considered_pos++;
+			}
+		    }
+		}
+		print $ofh "$chr\t$pos\tna\tna\tna\t$considered_pos\n";
+		print "$chr\t$pos\tna\tna\tna\t$considered_pos\n";
         }
  
 }
@@ -108,7 +130,6 @@ exit;
             upper=>$window,
             window=>$window,
             step=>$step,
-            file=>$file,
             fh=>$fh,
             curwin=>[],
             buffer=>[]
@@ -120,7 +141,7 @@ exit;
         my $self=shift;
         my $l=$self->_nextline();
         my $p=Utility::_parseLightwight($l);
-        my $c=scalar(@{$p->{samples}});
+        my $c=scalar(@{$p->{species}});
         $self->_bufferline($l);
         return $c;
     }
@@ -232,41 +253,84 @@ exit;
         my $data=shift;
 	my $pos=shift;
 	my $chr=shift;
-        
+        my $win=shift;
+	my $minCovFraction=shift;
+	
         my ($Dxy12,$Dxy13,$Dxy23) = (0,0,0);
         my ($ct12,$ct13,$ct23,$array_size) = (0,0,0,0);
         $array_size = @$data;
         
+	my $considered_pos = 0;
         foreach my $d (@$data)
         {
             #print "$d->{chr}\t$d->{pos}\t$d->{astate}\t$d->{dstate}\t$d->{sp1}\t$d->{sp2}\t$d->{sp3}\n";
-            if ("$d->{sp1}" ne "$d->{sp2}") {
-                $ct12++;
-            }
-            
-            if ("$d->{sp1}" ne "$d->{sp3}") {
-                $ct13++;
-            }
-            
-            if ("$d->{sp2}" ne "$d->{sp3}") {
-                $ct23++;
-            }
+	    ### if condition to discard the position with gap (-) in any of 3 species
+	    if (("$d->{sp1}" ne "-") and ("$d->{sp2}" ne "-") and ("$d->{sp3}" ne "-")) {
+		
+		if (($d->{sp1} !~ m/N/i) and ($d->{sp2} !~ m/N/i) and ($d->{sp3} !~ m/N/i)) {
+		
+			$considered_pos++;
+			
+			if ("$d->{sp1}" ne "$d->{sp2}") {
+			    $ct12++;
+			}
+			
+			if ("$d->{sp1}" ne "$d->{sp3}") {
+			    $ct13++;
+			}
+			
+			if ("$d->{sp2}" ne "$d->{sp3}") {
+			    $ct23++;
+			}
+		}
+	    }
             
         }
         
         # Calculate Dxy	    
-        $Dxy12 = $ct12/$array_size;
-        $Dxy13 = $ct13/$array_size;
-        $Dxy23 = $ct23/$array_size;
+        #$Dxy12 = $ct12/$array_size;
+        #$Dxy13 = $ct13/$array_size;
+        #$Dxy23 = $ct23/$array_size;
+	if(($considered_pos>0) and ($ct12>0)) {
+		$Dxy12 = $ct12/$considered_pos;
+		$Dxy12 = sprintf "%.10f",$Dxy12;
+	}
+	else {
+		$Dxy12 = "na";
+	}
+	
+	if(($considered_pos>0) and ($ct13>0)) {
+		$Dxy13 = $ct13/$considered_pos;
+		$Dxy13 = sprintf "%.10f",$Dxy13;
+	}
+	else {
+		$Dxy13 = "na";
+	}
+	
+	if(($considered_pos>0) and ($ct23>0)) {
+		$Dxy23 = $ct23/$considered_pos;
+		$Dxy23 = sprintf "%.10f",$Dxy23;
+	}
+	else {
+		$Dxy23 = "na";
+	}
+	
         
-        $Dxy12 = sprintf "%.10f",$Dxy12;
-        $Dxy13 = sprintf "%.10f",$Dxy13;
-        $Dxy23 = sprintf "%.10f",$Dxy23;
-
+        
+        
+	
+	my $string = "";
+	my $coveredFrac=$considered_pos/$win;
+	if ($coveredFrac>=$minCovFraction) {
+		
+		$string = "$chr\t$pos\t$Dxy12\t$Dxy13\t$Dxy23\t$considered_pos";
+	}
+	else {
+		$string = "$chr\t$pos\tna\tna\tna\t$considered_pos";
+	}
         #print $ofh "$chr\t$middle\t$Dxy12\t$Dxy13\t$Dxy23\n";
         
-        my $string = "";
-        $string = "$chr\t$pos\t$Dxy12\t$Dxy13\t$Dxy23";
+        
         return $string;
 
     }
@@ -309,21 +373,19 @@ exit;
         my @a=split /\s+/,$line;
         my $chr=shift @a;
         my $pos=shift @a;
-        my $astate=shift @a;
-        my $dstate=shift @a;
 	
 	my $en={};
+
 	if (($a[0] eq "-") or ($a[1] eq "-") or ($a[2] eq "-")) {
 
 	    $en={
 		chr=>$chr,
 		pos=>$pos,
-		astate=>$astate,
-		dstate=>$dstate,
 		iscov=>0,
 		sp1=>$a[0],
 		sp2=>$a[1],
-		sp3=>$a[2]
+		sp3=>$a[2],
+		species=>\@a
 	    };
         }
 
@@ -332,16 +394,15 @@ exit;
 	    $en={
 		chr=>$chr,
 		pos=>$pos,
-		astate=>$astate,
-		dstate=>$dstate,
 		iscov=>1,
 		sp1=>$a[0],
 		sp2=>$a[1],
-		sp3=>$a[2]
+		sp3=>$a[2],
+		species=>\@a
 	    };
 	    
 	}
-        
+     
         #$en->{ispuresnp} = ($en->{issnp} and not $taintedsnp)?1:0;
         
         return $en;
@@ -354,6 +415,193 @@ exit;
 
 
 
+{
+    package Test;
+    use strict;
+    use warnings;
+    #use Test::More;
+    #use Test::Simple;
+    
+    my $testcounter;
+    
+    sub _getBPSliderForString
+    {
+        my $str=shift;
+        my $window=shift;
+        my $step=shift;
+        open my $ofh,"<",\$str or die "could not open string filehandle";
+        my $cr=bless {   
+	    lower=>0,
+            upper=>$window,
+            window=>$window,
+            step=>$step,
+            fh=>$ofh,
+            curwin=>[],
+            buffer=>[]
+	    
+        },"BpSlider";
+        return $cr;
+    }
+    
+    sub runTests
+    {
+	$testcounter=1;
+	testCalculateDxy();
+	exit;
+    }
+    
+    
+
+    sub testCalculateDxy
+    {
+	
+	my $teststr=
+	"2L\t1\tA\t-\tA\n".
+        "2L\t2\tG\tC\tG\n".
+	"2L\t3\tG\t-\tG\n".
+	"2L\t4\tA\tG\tA\n".
+	"2L\t5\tC\tC\tG\n".
+	"2L\t6\tT\t-\tT\n".
+	"2L\t7\tA\tC\tA\n".
+	"2L\t8\tC\tN\tC\n".
+	"2L\t9\tC\tC\tC\n".
+	"2L\t10\tA\tG\tA\n"; #10
+	
+	my $minCoverageFraction = 0.6;
+	my $testfile = "test/dxytest.txt";
+        my $bpsl=_getBPSliderForString($teststr,10,10);
+	my $string="";
+		
+	#2L	249055	6	0.8333333333	na	0.8333333333
+        while( my $window=$bpsl->nextWindow() ) {
+
+		my $chr=$window->{chr};
+		my $pos=$window->{middle};
+		my $win=$window->{window};
+		my $above=$window->{count_covered};
+		my $data=$window->{data};
+		
+		next unless @$data;
+		
+		#print "TEST: $chr\t$pos\t$win\t$above\@$data\n";
+		
+		my $dxy = Test::calculateDxy($data,$pos,$chr,$win,$minCoverageFraction,$above);
+		
+		#print "$dxy->{chr}\t$dxy->{pos}\t$dxy->{considered_pos}\t$dxy->{D12}\t$dxy->{D13}\t$dxy->{D23}\n";
+
+		is($dxy->{chr},"2L","test CalculateDxy, correct chromosome");
+		is($dxy->{pos},5,"test CalculateDxy, correct window position");
+		is($dxy->{considered_pos},6,"test CalculateDxy, correct number of considered base pairs to calculate Dxy");
+		is($dxy->{D12},"0.6666666667","test CalculateDxy, the Dxy between species 1 and 2 is correct");
+		is($dxy->{D13},"0.1666666667","test CalculateDxy, the Dxy between species 1 and 3 is correct");
+		is($dxy->{D23},"0.8333333333","test CalculateDxy, the Dxy between species 2 and 3 is correct");
+	
+	}
+  
+    }
+    
+    
+    sub calculateDxy {
+	
+	my ($data,$pos,$chr,$win,$minCoverageFraction,$above) = (shift,shift,shift,shift,shift,shift);
+	
+	my $string = "";
+	my $coveredFrac=$above/$win;
+	my $dxy = {};
+	
+        if ($coveredFrac>=$minCoverageFraction) {
+	    
+	    $string = Utility::calculateDxy($data,$pos,$chr,$win,$minCoverageFraction);
+	    my ($chr,$pos,$D12,$D13,$D23,$considered_pos) = split("\t",$string);
+	    $dxy = {
+		chr=>$chr,
+		pos=>$pos,
+		D12=>$D12,
+		D13=>$D13,
+		D23=>$D23,
+		considered_pos=>$considered_pos
+	    };
+	    
+	}
+        else {
+		
+		my $considered_pos = 0;
+		foreach my $d (@$data)
+		{
+		    ### if condition to discard the position with gap (-) in any of 3 species
+		    if (("$d->{sp1}" ne "-") and ("$d->{sp2}" ne "-") and ("$d->{sp3}" ne "-")) {
+			if (($d->{sp1} =~ m/N/i) and ($d->{sp2} =~ m/N/i) and ($d->{sp3} =~ m/N/i)) {
+				$considered_pos++;
+			}
+		    }
+		}
+		$string = "$chr\t$pos\tna\tna\tna\t$considered_pos";
+		
+		$dxy = {
+		chr=>$chr,
+		pos=>$pos,
+		D12=>"na",
+		D13=>"na",
+		D23=>"na",
+		considered_pos=>$considered_pos
+	    };
+        }
+	
+	#return $string;
+	return $dxy;
+    }
+
+    sub is
+    {
+
+        my $a=shift;
+        my $b=shift;
+        my $msg=shift;
+	
+	
+        if($a eq $b)
+        {
+            print "$testcounter: OK $a = $b; $msg\n";
+        }
+        else
+        {
+            print "$testcounter: FAILED $a = $b; $msg\n";
+        }
+        $testcounter++;
+    }
+    
+    sub not_exists
+    {
+        my $a=shift;
+        my $msg=shift;
+        if($a)
+        {
+            print "$testcounter FAILED; $msg\n";
+        }
+        else
+        {
+            print "$testcounter OK: $msg\n";
+        }
+        $testcounter++;
+    }
+
+    sub ok
+    {
+        my $a=shift;
+        my $msg=shift;
+        if($a)
+        {
+            print "$testcounter OK: $msg\n";
+        }
+        else
+        {
+            print "$testcounter FAILED; $msg\n";
+        }
+        $testcounter++;
+    }
+    
+    
+}
 
 
 
@@ -364,12 +612,11 @@ calculate-dxy.pl - Calculates the distance between 2 species within a given wind
 
 =head1 SYNOPSIS
 
- perl calculate-dxy.pl --input mauve-parser-output.txt --output Dxy.txt --window-size 1000 --step-size 100\n";
+ perl calculate-dxy.pl --input mauve-parser-output.txt --output Dxy.txt --window-size 1000 --step-size 100
 
 =head1 OPTIONS
 
 =over 4
-
 
 =item B<--input>
 
@@ -403,17 +650,15 @@ Display help for this script
 
 A mauve-parser.pl program output file; example:
 
- 2L	5783	G	C	C	G	G
- 2L	5784	C	na	C	C	C
- 2L	5785	na	na	C	T	G
+ 2L	5783	C	G	G
+ 2L	5784	C	C	C
+ 2L	5785	C	T	G
 
  col 1: reference chromosome
  col 2: position in the reference chromosome
- col 3: ancestral allelic state
- col 4: derived allelic state
- col 5: allelic state in species 1 (reference species)
- col 6: allelic state in species 2
- col 7: allelic state in species 3 (outgroup species)
+ col 3: allelic state in species 1 (reference species)
+ col 4: allelic state in species 2
+ col 5: allelic state in species 3 (outgroup species)
  
 =head2 Output
 
